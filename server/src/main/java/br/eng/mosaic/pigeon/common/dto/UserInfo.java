4b0ab41639ed7db65cd19403ca16be9fa8c99487
Raoni Kulesza <raonikulesza@gmail.com>
2011-06-27 15:06:38 -0300

    fix UserSocialInfo and UserInfo constructor and add a partial install file to support env config

--- server/src/main/java/br/eng/mosaic/pigeon/common/dto/UserInfo.java
+++ server/src/main/java/br/eng/mosaic/pigeon/common/dto/UserInfo.java
@@ -6,6 +6,12 @@ public class UserInfo {
 	public String name;
 	public String token;
 	
+	public UserInfo(String id, String name) {
+		this.id = id;
+		this.name = name;
+
+	}
+	
 	public UserInfo(String id, String name, String token) {
 		this.id = id;
 		this.name = name;
