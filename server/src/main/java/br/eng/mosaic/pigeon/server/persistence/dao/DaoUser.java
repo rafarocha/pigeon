fccd177ed80dd935ba5dcfc44431bde190bed747
unknown <Dhiego@.(none)>
2011-07-02 16:03:48 -0300

    Refactoring retrieval top five method

--- server/src/main/java/br/eng/mosaic/pigeon/server/persistence/dao/DaoUser.java
+++ server/src/main/java/br/eng/mosaic/pigeon/server/persistence/dao/DaoUser.java
@@ -3,9 +3,12 @@
  */
 package br.eng.mosaic.pigeon.server.persistence.dao;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.SQLQuery;
 import org.hibernate.classic.Session;
 import org.hibernate.criterion.Order;
 
@@ -25,12 +28,21 @@ public class DaoUser extends DaoGeneric<String, User> {
 	public List<User> getTopFive() throws Exception {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		try {
-			Criteria c = session.createCriteria(User.class);
-			c.setMaxResults(5);
-			c.addOrder(Order.desc("score"));
+			SQLQuery sqlQuery = session.createSQLQuery("select u.email, u.name, u.score from User u order by u.score desc");
+			sqlQuery.setMaxResults(5);
+
+			List<?> list = sqlQuery.list();
+			List<User> result = new LinkedList<User>();
 			
-			List<User> users = c.list();
-			return users;
+			for (Object element : list) {
+				Object[] o = (Object[]) element;
+				User u = new User();
+				u.setEmail(o[0].toString());
+				u.setName(o[1].toString());
+				u.setScore(Double.parseDouble(o[2].toString()));
+				result.add(u);
+			}
+			return result;
 		} catch (Exception e) {
 			throw e;
 		}finally{
